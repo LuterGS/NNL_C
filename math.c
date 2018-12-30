1834574dadfd44b6d6b91306b58408f45576f437
LuterGS <koo04034@gmail.com>
2018-12-30 16:52:55 +0900

    Update math.c
    활성함수 / 미분된 활성함수 / 오차함수 넣어놓음

--- math.c
+++ math.c
@@ -2,3 +2,25 @@
 #include "declare.h"
 
 double reLU(double input){
+      if(input>0){ return input;}
+      else {return 0;}
+}
+
+double reLU_calculus(double input){
+      if(input>0) {return 1;}
+      else {return 0;}
+}
+
+double Leaky_reLU(double input){
+      if(input>0){return input;}
+      else {return input/10;}
+}
+
+double Leaky_reLU_calculus(double input){
+      if(input>0){ return 1;}
+      else {return 0.1;}
+}
+
+double square_error(double output, double answer){
+      return answer - output;
+}
